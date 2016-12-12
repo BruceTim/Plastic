8f505aef9084b259b49a950a0a4724c4333cfdd8
BruceTim <694040432@qq.com>
2016-12-12 19:29:30 +0800

    fix error in test Service

--- Codes/src/main/java/com/bruceTim/web/dao/EmailMapper.java
+++ Codes/src/main/java/com/bruceTim/web/dao/EmailMapper.java
@@ -1,11 +1,20 @@
 package com.bruceTim.web.dao;
 
 import com.bruceTim.core.generic.GenericDao;
+import com.bruceTim.web.enums.EmailState;
 import com.bruceTim.web.model.Email;
+
+import java.util.List;
 
 public interface EmailMapper extends GenericDao<Email, Long>{
 
     int insert (Email record);
 
     int updateByPrimaryKey (Email record);
+
+    Email selectByPrimaryKeySelective(Email email);
+
+    List<Email> selectByStateForPage(int state);
+
+    List<Email> selectAllForPage();
 }
