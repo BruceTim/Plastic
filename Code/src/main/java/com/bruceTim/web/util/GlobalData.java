62d2dbc86ab66fe013181af88b240f553a081e81
BruceTim <694040432@qq.com>
2016-12-25 17:31:39 +0800

    Add globalData

---
+++ Code/src/main/java/com/bruceTim/web/util/GlobalData.java
@@ -0,0 +1,13 @@
+package com.bruceTim.web.util;
+
+/**
+ * Created by BruceTim on 2016/12/19.
+ */
+public class GlobalData {
+
+    public static String FILE_UPLOAD_TYPE_CKEDITOR = "ckeditor";
+    public static String FILE_UPLOAD_PATH_CKEDITOR = "";
+    public static String FILE_UPLOAD_TYPE_BASE64 = "base64";
+    public static String FILE_UPLOAD_PATH_BASE64 = "";
+
+}
